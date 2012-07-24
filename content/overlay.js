f045c11474f7f22e56cd6a0e155a93ef71767df7
Vineeth Kashyap <vineeth@cs.ucsb.edu>
2012-07-24 15:12:20 -0700

    Initial commits.

---
+++ content/overlay.js
@@ -0,0 +1,44 @@
+
+var bwindow = {
+  num: 0,
+  
+  cur: 0,
+
+  bfind: null, 
+
+  next: function() {
+    bwindow.num = gBrowser.browsers.length; 
+    dump("calling next with: " + bwindow.cur + "of " + bwindow.num + "\n");
+    if (bwindow.cur >= bwindow.num) {
+      dump("resetting\n");
+      bwindow.cur = 0; // reset
+    }
+    if (!bwindow.bfind) {
+      dump("no bwindow.bfind\n");
+      var b =  gBrowser.getBrowserAtIndex(bwindow.cur);  
+      b.focus();
+      bwindow.bfind = b.webBrowserFind;
+      bwindow.bfind.searchString = "google";
+    }
+    try {
+    var result = bwindow.bfind.findNext();
+    } catch (e) {
+      result = false;
+    }
+    dump(result + "\n")
+    if (!result) {
+      dump("not found\n");
+      bwindow.cur += 1;
+      bwindow.bfind = null;
+    }
+  }
+}
+
+
+function runner(evt) {
+  if (evt.charCode === 97) { //a
+    bwindow.next();
+  }
+}
+
+window.addEventListener("keypress", runner, false);

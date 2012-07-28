eb2225917fe4d67144992548e85fd99c6e83a75d
Vineeth Kashyap <vineeth@cs.ucsb.edu>
2012-07-27 22:08:42 -0700

    Cleaned up code.
    1. Provided APIs for getting all tab indexes in which the search term was found.
    2. Provided new entry points for filling in the UI.

--- content/overlay.js
+++ content/overlay.js
@@ -1,25 +1,21 @@
 
+// the Search All Tabs Object
+// used as a namespace, so that we don't create global variables
 var SATO = {
   debug: function(str) {
     window.dump(str + "\n");
   },
 
-  fillWithTabTitles: function() {
-    var searchElement = document.getElementById("search-tabs-text-box");
-    var allTitles = SATO.getAllTabTitles().join("\n ");
-    window.dump("All titles = " + allTitles + "\n");
-    var foundTitles = SATO.getTabWithSearchTerms(searchElement.value).join("\n ");
-    window.dump("Search found titles = " + foundTitles + "\n");
-    var targetElement = document.getElementById("search-contents");
-    var allNode = document.createElement("p");
-    allNode.textContent = "All titles: " + allTitles;
-    var foundNode = document.createElement("p");
-    foundNode.textContent = "Found Titles: " + foundTitles;
-    targetElement.appendChild(allNode);
-    targetElement.appendChild(document.createElement("br"));
-    targetElement.appendChild(foundNode);
+  // entry point 
+  performSearch: function() {
+    var searchStr = document.getElementById("search-tabs-text-box").value;
+    // 1. SATO.getTabIndicesWithSearchTerms will give an array of indices
+    // 2. getGBrowser().getBrowserAtIndex(..) for each of those numbers will give browser elements
+    //    one can call .contentDocument.title/location etc to get information about that tab
+    // 3. getGBrowser().getselectTabAtIndex(..) will go to that tab 
   },
 
+  // returns a gBrowser
   getGBrowser: function() {
     var mainWindow = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)  
                        .getInterface(Components.interfaces.nsIWebNavigation)  
@@ -30,27 +26,25 @@ var SATO = {
     return mainWindow.gBrowser;
   },
 
+  // not to be used for now, perhaps later if we change our mind 
   getAllTabTitles: function() {
     var titles = [];
     var gBrowser = SATO.getGBrowser();
-    window.dump("Get all tab titles\n");
     for (var iter = 0; iter < gBrowser.browsers.length; ++iter) {
       titles.push(gBrowser.getBrowserAtIndex(iter).contentDocument.title); 
     }
     return titles;
   },
 
-  getTabWithSearchTerms: function(searchStr) {
-    return SATO.getAllTabsWithSearchTerms(searchStr).map(function(e) { return e.title; });
-  },
-
-  getAllTabsWithSearchTerms: function(searchStr) {
+  // returns an array of integers
+  // when these integers are plugged into gBrowser.getBrowserAtIndex()
+  // one gets the browser element of a tab that has the searchStr
+  getTabIndicesWithSearchTerms: function(searchStr) {
     var successTabs = [];
     var gBrowser = SATO.getGBrowser();
     for (var iter = 0; iter < gBrowser.browsers.length; ++iter) {
-      var cd = gBrowser.getBrowserAtIndex(iter).contentDocument; 
-      if (SATO.hasSearchItem(cd, searchStr)) {
-        successTabs.push(cd);
+      if (SATO.hasSearchItem(gBrowser.getBrowserAtIndex(iter).contentDocument, searchStr)) {
+        successTabs.push(iter);
       }
     }
     return successTabs;
@@ -59,55 +53,50 @@ var SATO = {
   // code modifed from this source:
   // http://www.borngeek.com/2011/10/03/using-the-nsifind-interface/
   hasSearchItem: function(doc, searchStr) {
-  // TODO: Note that in practice, we should do some error checking on
-  // the following three variables to make sure they are really available
-  
+    var body = doc.body; // Get a reference to the body element
+    var term = searchStr;
+    
+    // Create a highlighted span element which we will clone
+    // var span = doc.createElement("span");
+    // span.setAttribute("style", "background: #FF0; color: #000; " +
+    //                  "display: inline !important; font-size: inherit !important;");
+    
+    // Create our search range
+    var searchRange = doc.createRange();
+    searchRange.selectNodeContents(body);
+    
+    // Create the start point
+    var start = searchRange.cloneRange();
+    start.collapse(true); // Collapse to the beginning
+    
+    // Create the end point
+    var end = searchRange.cloneRange();
+    end.collapse(false); // Collapse to the end
+    
+    // Create the finder instance
+    var finder = Components.classes['@mozilla.org/embedcomp/rangefind;1']
+            .createInstance(Components.interfaces.nsIFind);
+    
+    if (finder.Find(term, searchRange, start, end)) {
+      return true;
+    }
+    else return false;
 
-  var body = doc.body; // Get a reference to the body element
-  
-  var term = searchStr; // The term to find, hard-coded for this example
-  
-  // Create a highlighted span element which we will clone
-  // var span = doc.createElement("span");
-  // span.setAttribute("style", "background: #FF0; color: #000; " +
-  //                  "display: inline !important; font-size: inherit !important;");
-  
-  // Create our search range
-  var searchRange = doc.createRange();
-  searchRange.selectNodeContents(body);
-  
-  // Create the start point
-  var start = searchRange.cloneRange();
-  start.collapse(true); // Collapse to the beginning
-  
-  // Create the end point
-  var end = searchRange.cloneRange();
-  end.collapse(false); // Collapse to the end
-  
-  // Create the finder instance
-  var finder = Components.classes['@mozilla.org/embedcomp/rangefind;1']
-          .createInstance(Components.interfaces.nsIFind);
-  
-  if (finder.Find(term, searchRange, start, end)) {
-    return true;
-  }
-  else return false;
-
-  // Perform the find operation
-  // while((start = finder.Find(term, searchRange, start, end)))
-  // {
-  //     window.dump(start + "\n");
-  //     // Clone the highlighter node and surround our search results with it
-  //     var hilitenode = span.cloneNode(true);
-  //     start.surroundContents(hilitenode);
-  // 
-  //     // Collapse the starting range to its end point, so we don't find this
-  //     // instance again the next time around the loop
-  //     start.collapse(false);
-  // 
-  //     // Workaround for Firefox bug #488427
-  //     body.offsetWidth;
-  // }
+    // Perform the find operation
+    // while((start = finder.Find(term, searchRange, start, end)))
+    // {
+    //     window.dump(start + "\n");
+    //     // Clone the highlighter node and surround our search results with it
+    //     var hilitenode = span.cloneNode(true);
+    //     start.surroundContents(hilitenode);
+    // 
+    //     // Collapse the starting range to its end point, so we don't find this
+    //     // instance again the next time around the loop
+    //     start.collapse(false);
+    // 
+    //     // Workaround for Firefox bug #488427
+    //     body.offsetWidth;
+    // }
 
   }
 };
